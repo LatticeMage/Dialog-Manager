d1c5fbd3d7aa73acf315f75ed802ab85634e7742
LatticeMage <LatticeMage@gmail.com>
2023-08-27 17:57:50 +0800

    increment success

--- Assets/DialogFlow Editor/BranchEditorWindow.cs
+++ Assets/DialogFlow Editor/BranchEditorWindow.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
+using System.IO;
 
 public class BranchEditorWindow : EditorWindow
 {
+    private string filePath = "Assets/DialogFlow Editor/test.txt";
+
     [MenuItem("Tools/Branch Editor")]
     public static void ShowWindow()
     {
@@ -24,12 +27,48 @@ public class BranchEditorWindow : EditorWindow
         var styles = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/DialogFlow Editor/BranchEditorStyles.uss");
         rootVisualElement.styleSheets.Add(styles);
 
-        // Load the text content from the file
-        string textContent = System.IO.File.ReadAllText("Assets/DialogFlow Editor/test.txt");
+        // Load the text content from the file and set it to the label
+        UpdateContentLabel();
 
-        // Find the label and set its text content
-        var contentLabel = rootVisualElement.Q<Label>("contentLabel");
-        contentLabel.text = textContent;
+        // Hook up the increment button click event
+        var incrementButton = rootVisualElement.Q<Button>("increment");
+        incrementButton.clicked += OnIncrementButtonClick;
+    }
+
+    private void UpdateContentLabel()
+    {
+        if (File.Exists(filePath))
+        {
+            string textContent = File.ReadAllText(filePath);
+            var contentLabel = rootVisualElement.Q<Label>("contentLabel");
+            contentLabel.text = textContent;
+        }
+        else
+        {
+            Debug.LogError($"File not found: {filePath}");
+        }
+    }
+
+    private void OnIncrementButtonClick()
+    {
+        if (File.Exists(filePath))
+        {
+            string content = File.ReadAllText(filePath);
+            if (int.TryParse(content, out int currentValue))
+            {
+                currentValue++;
+                File.WriteAllText(filePath, currentValue.ToString());
+                UpdateContentLabel(); // Update the label with the new value
+            }
+            else
+            {
+                Debug.LogError("Failed to parse content to integer.");
+            }
+        }
+        else
+        {
+            Debug.LogError($"File not found: {filePath}");
+        }
     }
 
     public void OnGUI()
